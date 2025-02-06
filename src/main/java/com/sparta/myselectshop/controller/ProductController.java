7af976c65466fbe747f83c84b26ffce054f0e6b9
Yeonnnny <kdoy030@gmail.com>
2025-02-06 10:17:59 +0900

    폴더 별 관심 상품 조회 오류 해결

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -57,7 +57,7 @@ public class ProductController {
         productService.addFolder(productId, folderId, userDetails.getUser());
     }
         
-    @GetMapping("/forders/{forderId}/products")
+    @GetMapping("/folders/{folderId}/products")
     public Page<ProductResponseDto> getProductsInFolder(
         @PathVariable Long folderId,
         @RequestParam("page") int page,
